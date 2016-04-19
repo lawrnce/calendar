a021f9e07248b6ffa9d5ac4c0e2ad5d16ef9f319
Lawrence Tran <lawrence@tran.com>
2016-04-19 16:17:03 -0500

    Added http server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
-console.log("HELLLOOOO");
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// For demo, store all Angular files for displaying
+app.use(express.static(__dirname+'/../client'));
+
+// Home page will be the calendar
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname+'/../client/views/index.html'));
+});
+
+app.get('/api/date', function(req, res) {
+
+});
+
+app.post('/api/date', function(req, res) {
+
+});
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
