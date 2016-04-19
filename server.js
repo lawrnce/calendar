7873f9d21906e47427e26f880c7f63bbaaaf5132
Lawrence Tran <lawrence@tran.com>
2016-04-19 17:02:36 -0500

    Added date format check

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var path = require('path');
+var moment = require('moment');
 
 // Connect to db
 mongoose.connect('mongodb://localhost/availabledates');
+
+// Date format
+var dateFormat = "YYYY-MM-DD";
 
 // Store static files
 app.use(express.static(__dirname+'/client'));
@@ -16,12 +20,17 @@ app.get('/', function(req, res) {
 
 // GET
 app.get('/api/date', function(req, res) {
-
 });
 
 // POST
 app.post('/api/date', function(req, res) {
+  // check if input is valid date format
+  var dateInput = req.body.date;
+  if (moment(dateInput, dateFormat).isValid()) {
 
+  } else {
+    return res.status(400).json({success: false, message: 'Invalid date format.'});
+  }
 });
 
 // DELETE
