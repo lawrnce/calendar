6fe3acea592f503039861ec947283798fa640262
Lawrence Tran <lawrence@tran.com>
2016-04-18 22:45:34 -0500

    Working templates on http server

--- client/app/directives/calendarDirective.js
+++ client/app/directives/calendarDirective.js
@@ -4,9 +4,8 @@
 angular.module('app.directive.calendar', [])
 	.directive('calendar', function() {
   		return {
-    		// restrict : 'E',
+    		restrict : 'E',
     		// scope: {},
-    		templateUrl : "../../views/templates/calendar.html"
-    		// template: "<h1>ASD</h1>"
+    		templateUrl : "templates/calendar.html"
   		};
 	});
