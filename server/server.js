13a5287440d84a81a64b0d032684fa940ef5a876
Lawrence Tran <lawrence@tran.com>
2016-04-19 16:28:34 -0500

    Template for api

--- server/server.js
+++ server/server.js
@@ -10,13 +10,20 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname+'/../client/views/index.html'));
 });
 
+// GET
 app.get('/api/date', function(req, res) {
 
 });
 
+// POST
 app.post('/api/date', function(req, res) {
 
 });
+
+// DELETE
+app.delete('/api/date', function(req, res) {
+
+})
 
 app.listen(3000, function() {
   console.log('Listening on port 3000');
